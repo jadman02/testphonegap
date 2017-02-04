4be1a361880d2f82daa408c883a958c6c10700de
jadman02 <jadman02@hotmail.com>
2017-02-04 23:19:54 +1100

    Update index.js

--- www/js/index.js
+++ www/js/index.js
@@ -84,7 +84,13 @@ view3 = myApp.addView('#view-3');
 view4 = myApp.addView('#view-4');
         myApp.init();
 
-
+ var popupHTML = '<div class="popup">'+
+                    '<div class="content-block">'+
+                      '<p>Popup created dynamically.</p>'+
+                      '<p><a href="#" class="close-popup">Close me</a></p>'+
+                    '</div>'+
+                  '</div>'
+  myApp.popup(popupHTML);
 
 
 $$('.panel-left').on('panel:opened', function () {
