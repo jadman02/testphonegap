b05fef522b3b733af49a794802cae723751c540c
jadman02 <jadman02@hotmail.com>
2017-06-18 13:43:58 +1000

    Update app.js

--- www/js/app.js
+++ www/js/app.js
@@ -5000,12 +5000,12 @@ $( ".onlineblock" ).show();
        //  ' <a href="#tab1" class="prevs button disabled" style="border-radius:5px;position:absolute;left:-37px;top:50%;margin-top:-28px;height:56px;width:56px;border:0;z-index:99;color:#2196f3;background-color:rgba(247, 247, 247, 0.952941);"><i class="pe-7s-angle-left pe-4x" style="margin-left:7px;margin-top:-1px;z-index:-1"></i></a>'+
        //                ' <a href="#tab3" class="nexts button" style="border-radius:5px;position:absolute;right:-37px;width:56px;top:50%;margin-top:-26px;height:56px;color:#2196f3;border:0;z-index:99;background-color:rgba(247, 247, 247, 0.952941);"><i class="pe-7s-angle-right pe-4x" style="margin-left:-35px;margin-top:-1px;"></i></a>'+
 
-     //onscroll="scrollQuestions();"
+
      
-'<div class="swiper-container swiper-questions" style="height:100%;overflow-y:scroll;">'+
+'<div class="swiper-container swiper-questions" style="height:100%;overflow-y:scroll;" onscroll="scrollQuestions();">'+
  
 
- '<div style="background: -webkit-linear-gradient(270deg, rgba(255,255,255,0) 0%, rgba(255,255,255,0.17) 17%, rgba(255,255,255,1) 100%);height:100%;width:100%;overflow-x:hidden;" onclick="backtoProfile();">'+
+ '<div style="height:100%;width:100%;overflow-x:hidden;" onclick="backtoProfile();">'+
  
 
 
