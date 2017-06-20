cfc5b81a527dbce8f2676b320aa85dc74e741018
jadman02 <jadman02@hotmail.com>
2017-06-20 16:30:29 +1000

    Update app.js

--- www/js/app.js
+++ www/js/app.js
@@ -4235,9 +4235,17 @@ new_all = singleuserarray;
 
 
    
-if (origin == 88){alert('88');}
-else if (origin == 1){alert('99');photoBrowser(0,singleuserarray[0].age,1,1);}
-else if (!origin){alert('100');photoBrowser(0,singleuserarray[0].age);}
+if (origin == 88){
+   alert('88');
+}
+else if (origin == 1){
+   alert('99');
+   photoBrowser(0,singleuserarray[0].age,1,1);
+}
+else if (origin != 88 && origin != 1){
+   alert('100');
+   photoBrowser(0,singleuserarray[0].age);
+}
 
 
   
