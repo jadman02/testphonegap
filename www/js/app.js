3a8923fedead362c919dac7e55a5b4df10649d38
jadman02 <jadman02@hotmail.com>
2017-06-25 11:44:32 +1000

    Update app.js

--- www/js/app.js
+++ www/js/app.js
@@ -649,15 +649,15 @@ if (!homewant || homewant =='offline'){
     $('.content-here').append(
     '<div class="no-results-div" style="text-align:center;margin:0 auto;width:300px;position:absolute;top:44px;left:50%;margin-left:-150px;margin-top:54px;">'+
     
-      '<h3>Get Quacking!</h3>'+
-       '<p style="padding-top:0px;margin-top:0px;">Are you looking to date, or duck or both? Your profile is not visible until you make a selection.</p>'+
+      '<h3>You are offline</h3>'+
+       '<p style="padding-top:0px;margin-top:0px;">Your profile will be active when you make a selection.</p>'+
        '<div class="row" style="border-top:1px solid #c4c4c4;padding-top:10px;padding-bottom:10px;">'+
     '<div class="col-30" style="padding-top:5px;"><img src="media/datefaceonly.png" style="width:80px;margin:0 auto;"></div>'+
-    '<div class="col-70" style="padding-top:5px;">Press <span style="font-family: \'Pacifico\', cursive;font-size:17px;">date</span> if you want to meet someone to hang out with, find a relationship or make conversation.</div>'+
+    '<div class="col-70" style="padding-top:5px;">Press <span style="font-family: \'Pacifico\', cursive;font-size:17px;color:white;">date</span> if you want to meet someone to hang out with, find a relationship or make conversation.</div>'+
 '</div>'+
               '<div class="row" style="padding-top:10px;padding-bottom:10px;">'+
     '<div class="col-30" style="padding-top:5px;"><img src="media/duckfaceonly.png" style="width:80px;margin:0 auto;"></div>'+
-    '<div class="col-70" style="padding-top:5px;">Press <span style="font-family: \'Pacifico\', cursive;font-size:17px;">duck</span> if you want to get down to...ahem...business (replace the D with an F). </div>'+
+    '<div class="col-70" style="padding-top:5px;">Press <span style="font-family: \'Pacifico\', cursive;font-size:17px;color:white;">duck</span> if you want to get down to...ahem...business (replace the D with an F). </div>'+
 '</div>'+
 
 
