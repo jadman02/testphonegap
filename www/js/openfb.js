780757ec667ec5f282af291c817527a3fccd8140
jadman02 <jadman02@hotmail.com>
2017-06-28 10:10:18 +1000

    Update openfb.js

--- www/js/openfb.js
+++ www/js/openfb.js
@@ -45,9 +45,11 @@ var openFB = (function () {
     // MAKE SURE YOU INCLUDE <script src="cordova.js"></script> IN YOUR index.html, OTHERWISE runningInCordova will always by false.
     // You don't need to (and should not) add the actual cordova.js file to your file system: it will be added automatically
     // by the Cordova build process
-    document.addEventListener("deviceready", function () {
-        runningInCordova = true;
-    }, false);
+   runningInCordova = true;
+    
+    //document.addEventListener("deviceready", function () {
+    //    runningInCordova = true;
+    //}, false);
 
     /**
      * Initialize the OpenFB module. You must use this function and initialize the module with an appId before you can
