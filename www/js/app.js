8ef1469d6225640efb98f1d8a74d53b1afed4818
jadman02 <jadman02@hotmail.com>
2017-06-30 14:19:50 +1000

    Update app.js

--- www/js/app.js
+++ www/js/app.js
@@ -1767,6 +1767,7 @@ if ($( "#distance_25" ).hasClass( "active" )){radiussize = '25';}
 if ($( "#distance_50" ).hasClass( "active" )){radiussize = '50';}
 if ($( "#distance_100" ).hasClass( "active" )){radiussize = '100';}
 
+
 availarray = [];
 $( ".availrec" ).each(function() {
 
@@ -1804,7 +1805,7 @@ var availstringn = availstring.toString();
 
 if ($('#soundnotif').prop('checked')) {offsounds = 'Y'} else {offsounds = 'N'}
 
-alert('66');
+
 //User Profile details
 
 var industry_u = $( "#industry-input" ).val();
@@ -1819,18 +1820,13 @@ var height_u = $( "#height-input" ).val().substring(0,3);
 var weight_pre = $( "#weight-input" ).val();
 var weight_u = weight_pre.substr(0, weight_pre.indexOf(' '));
 
-
-	} 
-	
+console.log(status_u);
 var uploadurl;
 
+	//if (f_largeurls.length > 0){photoresponse = 'Y';uploadurl = f_largeurls[0];}
+	//else{photoresponse='N';uploadurl = '';}
 
-if (f_largeurls.length > 0){photoresponse = 'Y';uploadurl = f_largeurls[0];}else{photoresponse='N';uploadurl = '';}
 
-	
-	
-
-alert('77');
 
 firebase.database().ref('users/' + f_uid).update({
     gender: newgender,
@@ -1847,13 +1843,13 @@ weight: weight_u,
     radius:radiussize,
     sort:sortby,
     availstring:availstring,
-    offsounds:offsounds
-//	photoresponse:photoresponse,
-//	uploadurl:uploadurl
+    offsounds:offsounds,
+	photoresponse:photoresponse,
+	uploadurl:uploadurl
     
     
   });
-alert('88');
+
 if (deletedphoto){
 
 var newsmall = f_smallurls.toString();
@@ -1894,12 +1890,12 @@ var height_u = $( "#height-input" ).val().substring(0,3);
 var weight_pre = $( "#weight-input" ).val();
 var weight_u = weight_pre.substr(0, weight_pre.indexOf(' '));
 
-alert('99');
+
 firebase.auth().currentUser.getToken().then(function(idToken) {
-$.post( "updatedetails.php", { projectid:f_projectid,token:idToken,currentid:firebase.auth().currentUser.uid,sexuality:sexuality,uid:f_uid,name:f_name,description:userzdescription,age:newage,availstring:availstringn,industry:industry_u,hometown:0,status:status_u,politics:politics_u,eyes:eyes_u,body:body_u,religion:religion_u,zodiac:zodiac_u,ethnicity:ethnicity_u,height:height_u,weight:weight_u} )
+$.post( "updatedetails.php", { projectid:f_projectid,token:idToken,currentid:firebase.auth().currentUser.uid,sexuality:sexuality,uid:f_uid,name:f_name,description:userzdescription,age:newage,availstring:availstringn,industry:industry_u,status:status_u,politics:politics_u,eyes:eyes_u,body:body_u,religion:religion_u,zodiac:zodiac_u,ethnicity:ethnicity_u,height:height_u,weight:weight_u} )
   .done(function( data ) {
-alert('didan update');
-alert(data);
+console.log('didan update');
+console.log(data);
 
 //if (f_gender && (f_gender != newgender)){
 //deleteDatabase(); 
@@ -10783,12 +10779,12 @@ var popupHTML = '<div class="popup prefpop">'+
 
  '   <div class="navbar-inner">'+
 
-  '      <div class="left" style="color:white;"><a href="#" onclick="updateUser();" style="color:white;display:none" class="donechange">Done</a><a href="#" style="color:white;display:none;" class="close-popup doneunchange">Done</a></div>'+
+  '      <div class="left" style="color:white;"></div>'+
    '     <div class="center swipetext" style="color:white;">Filters'+
          //'<div style="width:70px;height:70px;border-radius:50%;background-image:url(\''+f_image+'\');background-size:cover;background-position:50% 50%;margin-top:30px;z-index:100;border:5px solid #2196f3"></div>'+
 
    '</div>'+
-    '    <div class="right"></div>'+
+    '    <div class="right"><a href="#" onclick="updateUser();" style="color:white;display:none" class="donechange">Done</a><a href="#" style="color:white;display:none;" class="close-popup doneunchange">Done</a></div>'+
     
 
     
