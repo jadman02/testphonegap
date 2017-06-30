c2dee84bd602563aaad5497acefc941dbe8670c4
jadman02 <jadman02@hotmail.com>
2017-06-30 14:20:54 +1000

    Update app.js

--- www/js/app.js
+++ www/js/app.js
@@ -1892,7 +1892,7 @@ var weight_u = weight_pre.substr(0, weight_pre.indexOf(' '));
 
 
 firebase.auth().currentUser.getToken().then(function(idToken) {
-$.post( "updatedetails.php", { projectid:f_projectid,token:idToken,currentid:firebase.auth().currentUser.uid,sexuality:sexuality,uid:f_uid,name:f_name,description:userzdescription,age:newage,availstring:availstringn,industry:industry_u,status:status_u,politics:politics_u,eyes:eyes_u,body:body_u,religion:religion_u,zodiac:zodiac_u,ethnicity:ethnicity_u,height:height_u,weight:weight_u} )
+$.post( "updatedetails.php", { projectid:f_projectid,token:idToken,currentid:firebase.auth().currentUser.uid,hometown:0,sexuality:sexuality,uid:f_uid,name:f_name,description:userzdescription,age:newage,availstring:availstringn,industry:industry_u,status:status_u,politics:politics_u,eyes:eyes_u,body:body_u,religion:religion_u,zodiac:zodiac_u,ethnicity:ethnicity_u,height:height_u,weight:weight_u} )
   .done(function( data ) {
 console.log('didan update');
 console.log(data);
