0bfc0463343a498d487919ce5a090d4ff8ff7126
jadman02 <jadman02@hotmail.com>
2017-06-30 13:40:00 +1000

    Update app.js

--- www/js/app.js
+++ www/js/app.js
@@ -1899,28 +1899,7 @@ var uploadurl;
 	
 
 
-firebase.database().ref('users/' + f_uid).update({
-    gender: newgender,
-    industry:industry_u,
-	//hometown:hometown_u,
-    status:status_u,
-    politics: politics_u,eyes: eyes_u,body: body_u,religion: religion_u,zodiac: zodiac_u,ethnicity: ethnicity_u,
-height: height_u,
-weight: weight_u,
-        age: newage,
-    interested: newinterested,
-    lower: lowerage,
-    upper: upperage,
-    description:userzdescription,
-    radius:radiussize,
-    sort:sortby,
-    availstring:availstring,
-    offsounds:offsounds,
-	photoresponse:photoresponse,
-	uploadurl:uploadurl
-    
-    
-  });
+
 alert('13');
 	if (deletedphoto){
 
