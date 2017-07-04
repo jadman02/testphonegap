61c4c6c68df01800fb069fd331c6a706225a117a
jadman02 <jadman02@hotmail.com>
2017-07-04 14:27:22 +1000

    Update app.js

--- www/js/app.js
+++ www/js/app.js
@@ -847,9 +847,10 @@ if (!homewant || homewant =='offline'){
 	$( ".buttons-home" ).hide();
 	$( ".toolbar" ).hide();
 	$( ".results-loader" ).hide();
-    $('.content-here').append(
-    '<div class="no-results-div" style="text-align:center;margin:0 auto;width:300px;position:absolute;top:44px;left:50%;margin-left:-150px;margin-top:54px;">'+
     
+	$('.content-here').append(
+    '<div class="no-results-div" style="text-align:center;margin:0 auto;width:300px;position:absolute;top:44px;left:50%;margin-left:-150px;margin-top:54px;">'+
+    '<div class="heightquack">'
       '<h3>Get Quacking!</h3>'+
        '<div class="row" style="border-top:1px solid #c4c4c4;padding-top:10px;padding-bottom:10px;">'+
     '<div class="col-30" style="padding-top:5px;"><img src="media/datefaceonly.png" style="width:80px;margin:0 auto;"></div>'+
@@ -859,7 +860,9 @@ if (!homewant || homewant =='offline'){
     '<div class="col-30" style="padding-top:5px;"><img src="media/duckfaceonly.png" style="width:80px;margin:0 auto;"></div>'+
     '<div class="col-70" style="padding-top:5px;">Press <span style="font-family: \'Pacifico\', cursive;font-size:20px;">duck</span> if you want to get down to...ahem...business (replace the D with an F). </div>'+
 '</div>'+
-
+'</div>'+
+	    '<h3>What\'s New</h3>'+
+	    
 '<div class="swiper-container swiper-helper-info">'+
    ' <div class="swiper-wrapper">'+
      '   <div class="swiper-slide" style="height:300px;">Slide 1</div>'+
@@ -876,6 +879,8 @@ if (!homewant || homewant =='offline'){
 
     '</div>');
 
+	alert($( ".heightquack" ).height());
+	
     $( ".ploader" ).hide();
 
    
