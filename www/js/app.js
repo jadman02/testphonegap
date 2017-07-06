ba302bad1ab974af616f0552d561422b4800cf45
jadman02 <jadman02@hotmail.com>
2017-07-06 12:51:24 +1000

    Update app.js

--- www/js/app.js
+++ www/js/app.js
@@ -865,9 +865,10 @@ if (!homewant || homewant =='offline'){
 	 '</div>'+   
 
 		
-'<div class="swiper-container swiper-helper-info" style="background-color:#ccc;color:white;margin-left:-10px;margin-right:-10px;">'+
-   ' <div class="swiper-wrapper">'+
-     '   <div class="swiper-slide" style="height:'+swiperheight +'px;"><i class="pe-7s-like pe-3x"></i><h2>Efficient Matching</h2><p>Find people looking for the same thing as you.</p></div>'+
+'<div class="swiper-container swiper-helper-info" style="background-color:#ccc;color:#2196f3;margin-left:-10px;margin-right:-10px;padding-top:10px;">'+
+
+		' <div class="swiper-wrapper">'+
+     '   <div class="swiper-slide" style="height:'+swiperheight +'px;"><div style="position:absolute;top:50%;"><p><i class="pe-7s-like pe-3x"></i> Find people looking for the same thing as you.</p></div></div>'+
      '   <div class="swiper-slide" style="height:'+swiperheight +'px;"><i class="pe-7s-date pe-3x"></i><h2>Time saving</h2><p>Schedule meetings in your calendar and notify your matches of your availability.</p></div>'+
 		     '   <div class="swiper-slide" style="height:'+swiperheight +'px;"><i class="pe-7s-chat pe-3x"></i><h2>Real Life Interaction</h2><p>Chat is disabled until you both agree on a time to meet up until midnight of your date.</p></div>'+
 
@@ -875,7 +876,7 @@ if (!homewant || homewant =='offline'){
        
    ' </div>'+
 
-    '<div class="swiper-pagination-p"></div>'+
+    '<div class="swiper-pagination-p" style="margin-top:-20px;"></div>'+
 '</div>'+
    
 
