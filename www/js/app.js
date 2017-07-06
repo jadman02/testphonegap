ea365e0684cd541ae88e766e6b217071e233f456
jadman02 <jadman02@hotmail.com>
2017-07-07 08:47:40 +1000

    Update app.js

--- www/js/app.js
+++ www/js/app.js
@@ -1011,7 +1011,7 @@ for (q = 0; q < fetch.length; q++) {
 
 $.post( "http://www.dateorduck.com/locations.php", { want:homewant,projectid:f_projectid,token:idToken,currentid:firebase.auth().currentUser.uid,upper:f_upper,lower:f_lower,radius:radiussize,radiusunit:radiusunit,sexuality:sexuality,sortby:fetch[q],latitudep:latitudep,longitudep:longitudep} )
   .done(function( data ) {
-
+alert(JSON.stringify(data));
 fetched ++;
 
 
