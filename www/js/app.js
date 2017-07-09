038dfa0566d388e9bef9615f37fa813d897cbc22
jadman02 <jadman02@hotmail.com>
2017-07-09 22:46:52 +1000

    Update app.js

--- www/js/app.js
+++ www/js/app.js
@@ -2999,9 +2999,20 @@ var swiperno = 0;
         
         
               var blockindex = myPhotoBrowser.swiper.activeIndex;
+	 
+	 
+	 if ($('.infopopup').length > 0) {
+			
 swiperQuestions.removeSlide(blockindex);
+            swiperQuestions.updateSlidesSize();
+			
+			}
+	 else{
 	 myPhotoBrowser.swiper.removeSlide(blockindex);
             myPhotoBrowser.swiper.updateSlidesSize();
+	 }
+	 
+	 
 	 new_all = new_all.slice(0,blockindex).concat(new_all.slice(blockindex+1));
 
 	 if (new_all.length>0){
@@ -3186,10 +3197,19 @@ firebase.database().ref('matches/' + f_uid + '/' + targetid).update({
 
 }
  
+	 	if ($('.infopopup').length > 0) {
+			
+			 if (firstpos == 'Y'){allowedchange = true;swiperQuestions.swiper.slideNext();swiperQuestions.swiper.slidePrev();  }
+else if  (lastpos == 'Y'){allowedchange = true;swiperQuestions.swiper.slidePrev();swiperQuestions.swiper.slideNext();  }
+else {allowedchange = true;swiperQuestions.swiper.slideNext();swiperQuestions.swiper.slidePrev();} 
+			
+			}	       
+else{
+	 
  if (firstpos == 'Y'){myPhotoBrowser.swiper.slideNext();allowedchange = true;myPhotoBrowser.swiper.slidePrev();  }
 else if  (lastpos == 'Y'){myPhotoBrowser.swiper.slidePrev();allowedchange = true;myPhotoBrowser.swiper.slideNext();  }
 else {myPhotoBrowser.swiper.slideNext();allowedchange = true;myPhotoBrowser.swiper.slidePrev();} 
-	 
+}	 
 	
             
     
@@ -3276,7 +3296,6 @@ $( ".photo-browser-caption" ).append(targetdescription);
         
  if (new_all.length>0){
 	 checkMatch(targetid);
-	if ($('.infopopup').length > 0) {swiperQuestions.slideTo(myPhotoBrowser.swiper.activeIndex);}	       
 
 		      }
 	 
