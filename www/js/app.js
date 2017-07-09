4b493ea91219e83e5b77478dabc3db1a268b968a
jadman02 <jadman02@hotmail.com>
2017-07-10 09:48:12 +1000

    Update app.js

--- www/js/app.js
+++ www/js/app.js
@@ -3193,9 +3193,9 @@ firebase.database().ref('matches/' + f_uid + '/' + targetid).update({
  
 
 	 
- if (firstpos == 'Y'){myPhotoBrowser.swiper.slideNext();allowedchange = true;myPhotoBrowser.swiper.slidePrev();  }
-else if  (lastpos == 'Y'){myPhotoBrowser.swiper.slidePrev();allowedchange = true;myPhotoBrowser.swiper.slideNext();  }
-else {myPhotoBrowser.swiper.slideNext();allowedchange = true;myPhotoBrowser.swiper.slidePrev();} 
+ if (firstpos == 'Y'){myPhotoBrowser.swiper.slideNext();allowedchange = true;myPhotoBrowser.swiper.slidePrev();swiperQuestions.slideTo(myPhotoBrowser.swiper.activeIndex);  }
+else if  (lastpos == 'Y'){myPhotoBrowser.swiper.slidePrev();allowedchange = true;myPhotoBrowser.swiper.slideNext();swiperQuestions.slideTo(myPhotoBrowser.swiper.activeIndex);  }
+else {myPhotoBrowser.swiper.slideNext();allowedchange = true;myPhotoBrowser.swiper.slidePrev();swiperQuestions.slideTo(myPhotoBrowser.swiper.activeIndex); 
 	 
 	
             
