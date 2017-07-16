93ac95497e665d67541c7f732f8814b715b61e45
jadman02 <jadman02@hotmail.com>
2017-07-17 09:40:08 +1000

    Update app.js

--- www/js/app.js
+++ www/js/app.js
@@ -11448,8 +11448,9 @@ var popupHTML = '<div class="popup prefpop">'+
                   '  <div class="item-title-row">'+
                      '   <div class="item-title active button" style="border:0;border-radius:0px;font-size:17px;">Logout</div>'+
                     '</div>'+
-               ' </div>'+
-          '<div class="list-block-label hiderowpref" style="clear:both;float:left;width:100%;">This will log you out of the app, but not out of Facebook.</div>'+   
+              '<div class="list-block-label hiderowpref" style="clear:both;float:left;width:100%;">This will log you out of the app, but not out of Facebook.</div>'+   
+          
+    ' </div>'+
        
     ' </div>'+
     '</li>'+
