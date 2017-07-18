8e41a60c9de0248b527f2ff8767cf1f5ba97e652
jadman02 <jadman02@hotmail.com>
2017-07-19 09:36:03 +1000

    Update app.js

--- www/js/app.js
+++ www/js/app.js
@@ -151,9 +151,9 @@ try {
 	response1.summary.total_count;
 	
 	var friendstring;
-if (response1.summary.total_count ==0) {friendstring = '0 of your friends use Date or Duck'}
-	if (response1.summary.total_count ==1) {friendstring = '1 of your friends uses Date or Duck' }
-	if (response1.summary.total_count >1) {friendstring = response1.summary.total_count + ' of your friends use Date or Duck' }
+if (response1.summary.total_count ==0) {friendstring = 'No friends use this app'}
+	if (response1.summary.total_count ==1) {friendstring = '1 friend uses this app' }
+	if (response1.summary.total_count >1) {friendstring = response1.summary.total_count + ' friends use this app' }
 	
 
 	if (response1.summary.total_count > 9){
@@ -219,10 +219,28 @@ $('.recent-helper').html(
             '</div>'+
         '</li>'+
     '</ul></div>  ');
-		
+
+		$('.nearby-helper').html(
+		'<div class="list-block media-list" style="margin-top:5px;margin-bottom:5px;"><ul>'+
+     '   <li>'+
+          '  <div class="item-content">'+
+              // ' <div class="item-media">'+
+               //    ' <img src="path/to/img.jpg">'+
+              // ' </div>'+
+                '<div class="item-inner">'+
+                    '<div class="item-title-row">'+
+                      '  <div class="item-title">'+friendstring+'</div>'+
+                        '<div class="item-after"><a href="#" class="button button-big">Invite</a></div>'+
+                   ' </div>'+
+                    //<div class="item-subtitle">Subtitle</div>
+                   ' <div class="item-text">Invite <span class="badge" style="background-color:#ff3b30;color:white;">5</span> or more friends on Facebook to <br/>unlock this feature.</div>'+
+                '</div>'+
+            '</div>'+
+        '</li>'+
+    '</ul></div>  ');
 		
 		//$('.recent-helper').html('<p style="margin-top:-10px;padding:5px;">'+friendstring+'. Invite <span class="badge" style="background-color:#ff3b30;color:white;">10</span> or more friends on Facebook to <br/>unlock this feature.</p>');
- $('.nearby-helper').html('<p style=margin-top:-10px;"padding:5px;">'+friendstring+'. Invite <span class="badge" style="background-color:#ff3b30;color:white;">5</span> or more friends on Facebook to <br/>unlock this feature.</p>');
+// $('.nearby-helper').html('<p style=margin-top:-10px;"padding:5px;">'+friendstring+'. Invite <span class="badge" style="background-color:#ff3b30;color:white;">5</span> or more friends on Facebook to <br/>unlock this feature.</p>');
 //	$('.summary-helper').html('<p style="font-weight:bold;"></p><div class="row"><div class="col-50"><a class="button active external" href="sms:&body=Check out a new app in the App Store: https://fb.me/1554148374659639. It is called Date or Duck. Thoughts? ">Send SMS</a></div><div class="col-50"><a class="button active external" href="#" onclick="appLink()">Invite Friends</a></div></div><p style="color:#666;font-size:12px;margin-top:-10px;">We appreciate your help to grow this app!</p>');
 	
 	}
