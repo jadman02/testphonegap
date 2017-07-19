5bb06d8206dda1067bd84ac357bc5be81c3f99d0
jadman02 <jadman02@hotmail.com>
2017-07-19 11:09:11 +1000

    Update app.js

--- www/js/app.js
+++ www/js/app.js
@@ -204,7 +204,7 @@ recentshare = false;
 $('.recent-helper').html(
 		'<div class="list-block media-list" style="margin-top:-20px;margin-bottom:5px;"><ul>'+
      '   <li>'+
-          '  <div class="item-content" style="background-color:white;border-radius:5px;">'+
+          '  <div class="item-content" style="background-color:#ccc;border-radius:5px;">'+
               // ' <div class="item-media">'+
                //    ' <img src="path/to/img.jpg">'+
               // ' </div>'+
