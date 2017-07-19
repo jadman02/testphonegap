ba97cbae1eee65bd6aa3826189d2a593037c6c19
jadman02 <jadman02@hotmail.com>
2017-07-19 11:48:45 +1000

    Update app.js

--- www/js/app.js
+++ www/js/app.js
@@ -130,6 +130,8 @@ function newHometown(){
 
 function fQuery(){
 
+	$( ".summary-helper" ).show(); 
+	
 $.ajax({
    url: "https://graph.facebook.com/v2.4/784956164912201?fields=context.fields(friends_using_app)",
     type: "get",
@@ -1929,6 +1931,7 @@ recentswiper.destroy();
           
           $( ".nearby-helper" ).hide(); 
                     $( ".recent-helper" ).hide(); 
+	$( ".summary-helper" ).hide(); 
           
 $( ".results-loader" ).show();          
 getWifilocation();
