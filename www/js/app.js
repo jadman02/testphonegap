8254a7a821703ae7877ee5df59ae8d293533ce86
jadman02 <jadman02@hotmail.com>
2017-07-27 11:35:27 +1000

    Update app.js

--- www/js/app.js
+++ www/js/app.js
@@ -10497,7 +10497,7 @@ notifloaded = true;
 //Update SQL notifcount	
 
 
-cordova.plugins.notification.badge.set(notificationscount);
+cordova.plugins.notification.badge.set(103);
 
 firebase.auth().currentUser.getToken().then(function(idToken) { 
 $.post( "http://www.dateorduck.com/updatenotifications.php", { projectid:f_projectid,token:idToken,currentid:firebase.auth().currentUser.uid,uid:f_uid,notifcount:notificationscount} )
