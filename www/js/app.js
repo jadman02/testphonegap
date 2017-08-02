a5c0c1ab3ad685b90f8b20246970c33f9bb953fd
jadman02 <jadman02@hotmail.com>
2017-08-03 09:56:53 +1000

    Update app.js

--- www/js/app.js
+++ www/js/app.js
@@ -11517,7 +11517,9 @@ var popupHTML = '<div class="popup prefpop">'+
 
   '<div id="tab99" class="view-99 view tab active">'+
 
-    '<div class="toolbar tabbar swipetoolbar" style="background-color:#ccc;z-index:9999999999;position:absolute;bottom:0px;">'+
+    //z-index:9999999999;position:absolute;bottom:0px;
+    
+    '<div class="toolbar tabbar swipetoolbar" style="background-color:#ccc;">'+
 ' <div class="toolbar-inner" style="padding:0;">'+
 
        //   '   <a href="#" class="button tab-link tab-swipe pan0 active" onclick="swipePref(0)" style="border-radius:0;font-size:17px;border:0;text-align:center;"><i class="pe-7s-filter pe-lg" style="width:22px;margin:0 auto;"></i></a>'+
