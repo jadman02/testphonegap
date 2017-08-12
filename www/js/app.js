05ff89a19c8b1aa027282bca9f0c3219817ec520
jadman02 <jadman02@hotmail.com>
2017-08-13 09:02:45 +1000

    Update app.js

--- www/js/app.js
+++ www/js/app.js
@@ -40,7 +40,7 @@ if (param == 3){titlestring = 'New date confirmed';bodystring='By ' + f_first;}
 		 if (d_type){typesend = d_type;}
 		 else {typesend = 'date';}
 		 
-	$.post( "http://www.dateorduck.com/sendnotification.php", {projectid:f_projectid,token:idToken,currentid:firebase.auth().currentUser.uid,target:targetto,titlestring:titlestring,bodystring:bodystring,param:param,type:typesend,firstname:f_first} )
+	$.post( "http://www.dateorduck.com/sendnotification.php", {projectid:f_projectid,token:idToken,currentid:firebase.auth().currentUser.uid,target:targetto,titlestring:titlestring,bodystring:bodystring,param:param,type:typesend,firstname:f_first,senderid:f_uid} )
 
   .done(function( data ) {
 		//alert(JSON.stringify(data));
@@ -1491,7 +1491,7 @@ recent_all = [];
 		'    <div class="content-block-title" style="width:100%;text-align:center;margin-top:20px;margin-bottom:10px;margin-left:0px;">Support this app</div>'+
 '<a href="#" class="button-big button active" style="margin-bottom:10px;" onclick="appLink()">Invite Friends</a>'+
 		'<a href="#" class="button-big button" style="margin-bottom:10px;" onclick="sharePop()">Share</a>'+
-		'<a class="button-big button external" href="sms:&body=Check out a new app in the App Store: https://fb.me/1554148374659639. It is called Date or Duck. Thoughts? " style="margin-bottom:10px;">Send SMS</a>'+
+		'<a class="button-big button external" href="sms:&body=Check out this new a new app in the App Store: https://fb.me/1554148374659639. It is called Date or Duck. Thoughts? " style="margin-bottom:10px;">Send SMS</a>'+
     '</div>');
 
 
