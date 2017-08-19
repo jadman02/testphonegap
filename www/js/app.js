239a1dc52bfd6a2e07738c22809d950f238086bb
jadman02 <jadman02@hotmail.com>
2017-08-19 22:23:14 +1000

    Update app.js

--- www/js/app.js
+++ www/js/app.js
@@ -2146,7 +2146,7 @@ deletePhotos();
 function justGeo(){
 
 firebase.auth().currentUser.getToken().then(function(idToken) {   
-$.post( "http://www.dateorduck.com/updatelocation.php", { projectid:f_projectid,token:idToken,currentid:firebase.auth().currentUser.uid,uid:f_uid,latitude:latitudep,longitude:longitudep} )
+$.post( "http://www.dateorduck.com/updatelocation.php", { system:'ios',projectid:f_projectid,token:idToken,currentid:firebase.auth().currentUser.uid,uid:f_uid,latitude:latitudep,longitude:longitudep} )
 
   .done(function( data ) {
   
@@ -2171,7 +2171,7 @@ function updateGeo(){
 firebase.auth().currentUser.getToken().then(function(idToken) {  
 	
 	
-$.post( "http://www.dateorduck.com/updatelocation.php", { projectid:f_projectid,token:idToken,currentid:firebase.auth().currentUser.uid,uid:f_uid,latitude:latitudep,longitude:longitudep} )
+$.post( "http://www.dateorduck.com/updatelocation.php", { system:'ios',projectid:f_projectid,token:idToken,currentid:firebase.auth().currentUser.uid,uid:f_uid,latitude:latitudep,longitude:longitudep} )
 
   .done(function( data ) {
 	
